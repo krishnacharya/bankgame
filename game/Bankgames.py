42ac35b8c28e2e52ee9d2c989e9b8438db5401e0
KVA <krishnacharya97@gmail.com>
2025-01-22 23:13:38 -0800

    adds moving avg

--- game/Bankgames.py
+++ game/Bankgames.py
@@ -64,7 +64,44 @@ class GameFreshEstimate:
             b1_record.append(p_b1)
             b2_record.append(p_b2)
         return np.array(b1_record), np.array(b2_record)
-        
 
-class GameMovingAverage:# TODO
-    pass
+
+class GameMovingAvg:
+    def __init__(self, gammas:list[float], taus: list[float], num_samples:int, dist:Dist):
+        '''
+            num_samples: number of customers to draw in each round.
+        '''
+        self.gammas = gammas
+        self.taus = taus
+        self.num_samples = num_samples
+        self.dist = dist
+        self.num_rounds = 0 # number of rounds of hedge
+        self.A_est = np.zeros(())
+
+    def update_PayoffMat_est(self):
+        y_samples = self.dist.get_samples(self.num_samples)
+        current_matrix = matrix_from_samples(y_samples=y_samples, gammas=self.gammas, taus=self.taus)
+        self.num_rounds += 1
+        self.A_est = self.A_est + (current_matrix - self.A_est) / self.num_rounds
+        return self.A_est
+
+    def run_hedge(self, T:int, p_b1, p_b2, eta):
+        '''
+            T: number of rounds of hedge
+            p_b1: initial probability weights on each action for bank1 
+            p_b2: initial probability weights on each action for bank1 
+
+            returns 
+            probability on each action in each round
+            
+            return type -  (T, #actions), (T, #actions)
+            Bank 1, Bank2
+        '''
+        b1_record = [p_b1]
+        b2_record = [p_b2]
+        for t in range(T):
+            self.update_PayoffMat_est()
+            p_b1, p_b2 = HedgeSimultaneous(p_b1, p_b2, eta, self.A_est)
+            b1_record.append(p_b1)
+            b2_record.append(p_b2)
+        return np.array(b1_record), np.array(b2_record)
