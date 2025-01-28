70f9bd23074c6e2e9b079350bc988df26cc48704
Krishna V Acharya <kacharya33@login-phoenix-slurm-4.pace.gatech.edu>
2025-01-28 18:37:04 -0500

    adds large T for tg

--- slurmjobs_2gamma/launch_tg_jobs.sh
+++ slurmjobs_2gamma/launch_tg_jobs.sh
@@ -4,10 +4,10 @@
 #SBATCH -A gts-jziani3
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=2:00:00
-#SBATCH --output=./Sbatch-tg/tg2gam.%A\_%a.out
-#SBATCH --error=./Sbatch-tg/tg2gam.%A\_%a.error
+#SBATCH --output=./Sbatch-tg2/tg2gam.%A\_%a.out
+#SBATCH --error=./Sbatch-tg2/tg2gam.%A\_%a.error
 
-mkdir -p ./Sbatch-tg
+mkdir -p ./Sbatch-tg2
 
 module load anaconda3/2022.05.0.1
 conda activate bankgame
@@ -20,7 +20,7 @@ mu=$(echo ${iteration} | cut -d "," -f 1)
 sigma=$(echo ${iteration} | cut -d "," -f 2)
 gamma_l=$(echo ${iteration} | cut -d "," -f 3)
 gamma_h=$(echo ${iteration} | cut -d "," -f 4)
-horizon=100000
+horizon=500000
 eta=0.1
 num_startprofiles=5
 
